7fd03d6f2f3432987a2a79a0afea0f0dde0b0f6f
NikithaMandhana <145818235+NikithaMandhana@users.noreply.github.com>
2023-10-14 07:15:11 -0700

    Committing the changes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,48 +1,18 @@
-import { ModuleWithProviders, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { StudentComponent } from './student/student.component';
-import { FacultyComponent } from './faculty/faculty.component';
-import { AdminComponent } from './admin/admin.component';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { DepartmentComponent } from './department/department.component';
-import { LoginComponent } from './login/login.component';
-import { LandingPageComponent } from './landing-page/landing-page.component';
-import { MatButtonModule } from '@angular/material/button';
-import { MatInputModule } from '@angular/material/input';
-import { PaymentComponent } from './payment/payment.component';
-import { MatTableModule } from '@angular/material/table';
-import { RegisterationComponent } from './registeration/registeration.component';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    StudentComponent,
-    FacultyComponent,
-    AdminComponent,
-    DepartmentComponent,
-    LoginComponent,
-    LandingPageComponent,
-    PaymentComponent,
-    RegisterationComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    NoopAnimationsModule,
-    FormsModule,
-    HttpClientModule,
-    MatButtonModule,
-    MatInputModule,
-    MatTableModule,
-    MatFormFieldModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {
-}
+export class AppModule { }
